d05419988e58b65bada0c34bac97158cfcc813f4
Juan Gabriel Gutierrez <correo.del.llanox@gmail.com>
2015-06-09 16:30:25 -0500

    Adding download manager

---
+++ Libraries/Image/RCTDownloadManager.h
@@ -0,0 +1,17 @@
+//
+//  RCTDownloadManager.h
+//  GoldfingrNative
+//
+//  Created by Juan Gabriel Gutierrez on 2015-06-09.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import  "RCTBridge.h"
+@class RCTImageDownloader;
+
+@interface RCTDownloadManager :  NSObject <RCTBridgeModule>
+
+//@class RCTImageDownloader;
+
+#pragma mark - Util_Methods
+@end
---
+++ Libraries/Image/RCTDownloadManager.m
@@ -0,0 +1,58 @@
+//
+//  RCTDownloadManager.m
+//  GoldfingrNative
+//
+//  Created by Juan Gabriel Gutierrez on 2015-06-09.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import "RCTDownloadManager.h"
+#import "RCTImageDownloader.h"
+#import "RCTCache.h"
+#import "RCTLog.h"
+
+@implementation RCTDownloadManager
+{
+  RCTImageDownloader *_imageDownloader;
+  id _downloadToken;
+}
+
+   RCT_EXPORT_MODULE();
+
+   RCT_EXPORT_METHOD(downloadImageWithUrl:(NSURL *)url response:(RCTResponseSenderBlock)callback){
+    
+     
+     _imageDownloader = [RCTImageDownloader sharedInstance];
+    
+     [_imageDownloader downloadDataForURL:url block:^(NSData *data, NSError *error) {
+       
+      if (error) {
+         RCTLogWarn(@"Unable to download image data. Error: %@", error);
+         callback(@[[NSNull null], @"Error"]);
+         return;
+       }
+       
+       if (data) {
+         RCTLogWarn(@"Downloaded image data. Data: %@", data);
+         callback(@[[NSNull null], @"Downloaded image data"]);
+         return;
+       }
+       
+       
+     }];
+
+     
+    
+    
+     
+   }
+
+
+  RCT_EXPORT_METHOD(listDownloadedData:(RCTResponseSenderBlock)callback){
+  
+   NSMutableArray *serializedContacts = [NSMutableArray new];
+   callback(@[[NSNull null], serializedContacts]);
+  
+}
+
+@end
